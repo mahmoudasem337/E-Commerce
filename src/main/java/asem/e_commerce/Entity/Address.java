57ba1947d7fb97139b38a05272c048df7ab0b1b0
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-11 18:13:25 +0300

    added address entity

---
+++ src/main/java/asem/e_commerce/Entity/Address.java
@@ -0,0 +1,61 @@
+package asem.e_commerce.Entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("street_name")
+    private String street_name;
+    @JsonProperty("home_number")
+    private int home_number;
+
+    public Address() {
+    }
+
+    public Address(int id, String city, String street_name, int home_number) {
+        this.id = id;
+        this.city = city;
+        this.street_name = street_name;
+        this.home_number = home_number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public int getHome_number() {
+        return home_number;
+    }
+
+    public void setHome_number(int home_number) {
+        this.home_number = home_number;
+    }
+}
+
