690472a926f428c4117a831eaf3420edae89867f
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-11 18:14:05 +0300

    added address repository

---
+++ src/main/java/asem/e_commerce/Repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package asem.e_commerce.Repository;
+
+import asem.e_commerce.Entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address,Integer> {
+}
+
