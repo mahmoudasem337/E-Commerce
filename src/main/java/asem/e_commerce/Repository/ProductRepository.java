2098e795a125f317d7a6dbb5faa755d092a209d8
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-12 18:14:04 +0300

    Create Product Repository

---
+++ src/main/java/asem/e_commerce/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package asem.e_commerce.Repository;
+
+import asem.e_commerce.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
