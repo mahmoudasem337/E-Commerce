167701130f6ee69ee0366ae25b358244b374ff57
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-12 18:13:06 +0300

    Create Product Entity

---
+++ src/main/java/asem/e_commerce/Entity/Product.java
@@ -0,0 +1,76 @@
+package asem.e_commerce.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String product_name;
+    private String description;
+    private int price;
+    private double discount;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(int id, String product_name, String description, int price, double discount, int quantity) {
+        this.id = id;
+        this.product_name = product_name;
+        this.description = description;
+        this.price = price;
+        this.discount = discount;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
